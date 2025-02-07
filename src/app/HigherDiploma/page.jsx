1b3083e5fb37210b65a43b15102192459dbfe70f
DineshGarusinghege <ictdinesh.se@gmail.com>
2025-02-08 04:35:50 +0530

    bgcolorremoveandunnessoryfiledelete

--- src/app/HigherDiploma/page.jsx
+++ src/app/HigherDiploma/page.jsx
@@ -51,48 +51,6 @@ export default function HigherDiploma() {
 
 
 
-    const testimonials = [
-        {
-            image: "/images/young-female-indian-collage-girl-going-college-smiling_437792-127.avif",
-            text: "“The continuous support, help, advice, and guidance given by the management of the campus can never go unnoticed as they fully committed themselves to deliver their promises to accomplish the objective of becoming a recognized graduate...”",
-            name: "Azeem",
-            buttonLabel: "Read Azeem's Story",
-        },
-        {
-            image: "/images/young-indian-college-girl-smiling_54391-7128.avif",
-            text: "“The campus environment and the faculty's dedication made a huge difference in my career path. I am truly grateful to BIMT.”",
-            name: "Michael",
-            buttonLabel: "Read Michael's Story",
-        },
-        {
-            image: "/images/smiling-woman-holding-orange-planners-notebooks-one-arm-her-other-arm-is-holding_878783-7373.avif",
-            text: "“BIMT provided me with the opportunity to grow academically and professionally. The hands-on experiences and resources were phenomenal.”",
-            name: "Sara",
-            buttonLabel: "Read Sara's Story",
-        },
-        {
-            image: "/images/young-asian-indian-student-with-glasses-backpack-holds-book-shows-thumbs-up_928503-91.avif",
-            text: "“The campus environment and the faculty's dedication made a huge difference in my career path. I am truly grateful to BIMT.”",
-            name: "Michael",
-            buttonLabel: "Read Michael's Story",
-        },
-
-    ];
-
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    const handleNext = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % testimonials.length);
-    };
-
-    const handlePrev = () => {
-        setCurrentIndex((prevIndex) =>
-            prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1
-        );
-    };
-
-    const { image, text, buttonLabel } = testimonials[currentIndex];
-
 
     const studyPrograms = [
         {
@@ -122,30 +80,6 @@ export default function HigherDiploma() {
         },
     ];
 
-    // const courses = [
-    //     {
-    //         id: 1,
-    //         category: 'Higher Diploma',
-    //         title: 'Higher Diploma in',
-    //         description: 'Accounting and FInance',
-    //         img: '/images/3 (1) 1.png',
-    //     },
-    //     {
-    //         id: 2,
-    //         category: 'Higher Diploma',
-    //         title: 'Higher Diploma in',
-    //         description: 'Business Management',
-    //         img: '/images/3 (1) 1 (1).png',
-    //     },
-    //     {
-    //         id: 3,
-    //         category: 'Higher Diploma',
-    //         title: 'Higher Diploma in',
-    //         description: 'Higher Diploma in Computing',
-    //         img: '/images/3 (1) 1 (2).png',
-    //     },
-
-    // ];
     const [query, setQuery] = useState("");
     const [suggestions, setSuggestions] = useState([]);
 
@@ -378,7 +312,7 @@ export default function HigherDiploma() {
 
             {/* Management Deploma Courses */}
 
-            <div className="bg-[#F6F8FB] py-10 px-4 lg:px-16 pt-[150px]">
+            <div className="py-10 px-4 lg:px-16 pt-[150px]">
                 {/* Header Section */}
                 <div className="text-center mb-8">
                     <h3 className="text-lg font-medium text-gray-500"
@@ -476,95 +410,9 @@ export default function HigherDiploma() {
             </div>
 
 
-            <div className="max-w-[1457px] mx-auto px-6 py-10 pt-[150px]">
-                <div className="flex flex-col lg:flex-row items-start gap-5 relative">
-                    {/* Left Side Image */}
-                    <div className="flex-shrink-0">
-                        <img
-                            src="/images/front-view-male-student-wearing-black-backpack-holding-copybooks-files-blue-wall_140725-42637 1 (1).png"
-                            alt="Student"
-                            className="w-full h-auto rounded-lg shadow-lg"
-                            style={{
-                                width: '800px',
-                                height: '532.907px',
-                                flexShrink: '0'
-                            }}
-                        />
-                    </div>
-
-                    {/* Right Side Content */}
-                    <div className="bg-white shadow-lg rounded-lg p-8 absolute left-[650px] w-[781px] h-[457px] top-[38px]">
-                        <h4 className="text-blue-600 text-lg font-bold mb-2"
-                            style={{
-                                color: '#272A5D',
-                                fontFamily: "Work Sans",
-                                fontSize: '24px',
-                                fontStyle: 'normal',
-                                fontWeight: '400',
-                                lineHeight: 'normal'
-                            }}
-                        >
-                            Why BIMT
-                        </h4>
-                        <h2 className="text-2xl lg:text-3xl font-bold mb-4"
-                            style={{
-                                color: '#272A5D',
-                                fontFamily: 'Avenir LT Std',
-                                fontSize: '40px',
-                                fontStyle: 'normal',
-                                fontWeight: '600',
-                                lineHeight: "53px"
-                            }}
-                        >
-                            WHY WE’RE THE RIGHT CHOICE FOR YOU
-                        </h2>
-                        <p className="text-gray-600 leading-relaxed"
-                            style={{
-                                color: '#5A5A5A',
-                                textAlign: "justify",
-                                fontFamily: 'Work Sans',
-                                fontSize: "16px",
-                                fontStyle: 'normal',
-                                fontWeight: '400',
-                                lineHeight: 'normal'
-                            }}
-                        >
-                            BIMT Campus was established in order to electrify the employability of
-                            the Nation. We are equipped with the best panel of lecturers who are not
-                            only qualified professionally and academically but also enriched with
-                            global and local corporate competence. We have created a unique learning
-                            environment that will empower our students with the necessary skills to
-                            achieve their corporate ambitions. Their success depends upon the
-                            applications of theoretical skills and practical experience. It is the
-                            combination, where we are true experts, ensuring that students get the
-                            right balance of solid theory coupled with necessary real-life case
-                            studies and teaching.
-                        </p>
-                        <p className="text-gray-600 leading-relaxed mt-4"
-                            style={{
-                                color: '#5A5A5A',
-                                textAlign: "justify",
-                                fontFamily: 'Work Sans',
-                                fontSize: "16px",
-                                fontStyle: 'normal',
-                                fontWeight: '400',
-                                lineHeight: 'normal'
-                            }}
-                        >
-                            BIMT CAMPUS is the fastest growing Private Sector Higher Educational
-                            Institute in Sri Lanka. We offer a range of internationally Recognized
-                            Educational programs in partnership with globally reputed Universities
-                            and Awarding bodies. We have picked the best courses, which offer the
-                            qualifications suitable for Sri Lankan students to gain complete
-                            knowledge on subject matters.
-                        </p>
-                    </div>
-                </div>
-            </div>
-
 
             {/* study card start section */}
-            <div className="bg-[#F6F8FB] py-10 px-4 lg:px-16">
+            <div className="py-10 px-4 lg:px-16">
                 {/* Header Section */}
                 <div className="text-center mb-8">
                     <h3 className="text-lg font-medium text-gray-500"
@@ -632,141 +480,6 @@ export default function HigherDiploma() {
                 </div>
             </div>
             {/* Study cards end section */}
-
-            <div
-                className="bg-[#006987] flex flex-col items-center py-16"
-                style={{ marginTop: '110px', marginBottom: '100px' }}
-            >
-                {/* Outer Container for Content with Fixed Width */}
-                <div
-                    className="w-[1349px] mx-auto flex flex-col lg:flex-row items-center lg:items-start"
-                >
-                    {/* Image Section */}
-                    <div className="relative lg:mr-16 mb-8 lg:mb-0">
-                        <Image
-                            src={image}
-                            alt="Alumni"
-                            width={438}
-                            height={498}
-                            className="rounded-lg"
-                        />
-                        <div className="flex justify-center mt-4 space-x-4">
-                            <button
-                                onClick={handlePrev}
-                                className="w-12 h-12 flex items-center justify-center rounded-full bg-transparent border-2 border-white text-white"
-                            >
-                                {/* Left Arrow SVG */}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
-                                    <g clip-path="url(#clip0_327_4530)">
-                                        <path d="M25 0C38.8068 0 50 11.1932 50 25C50 38.8068 38.8068 50 25 50C11.1932 50 0 38.8068 0 25C0 11.1932 11.1932 0 25 0ZM25 45.8333C36.5057 45.8333 45.8333 36.5057 45.8333 25C45.8333 13.4943 36.5057 4.1667 25 4.1667C13.4943 4.1667 4.1667 13.4943 4.1667 25C4.1667 36.5057 13.4943 45.8333 25 45.8333Z" fill="white" />
-                                        <path d="M27.6936 13.1102C28.5071 12.2966 29.8263 12.2966 30.6398 13.1102C31.4534 13.9238 31.4534 15.2429 30.6398 16.0565L21.6963 25L30.6398 33.9436C31.4534 34.7572 31.4534 36.0763 30.6398 36.8899C29.8263 37.7034 28.5071 37.7034 27.6936 36.8899L17.2769 26.4732C16.4633 25.6596 16.4633 24.3405 17.2769 23.5269L27.6936 13.1102Z" fill="white" />
-                                    </g>
-                                    <defs>
-                                        <clipPath id="clip0_327_4530">
-                                            <rect width="50" height="50" fill="white" transform="matrix(-1 0 0 1 50 0)" />
-                                        </clipPath>
-                                    </defs>
-                                </svg>
-                            </button>
-                            <button
-                                onClick={handleNext}
-                                className="w-12 h-12 flex items-center justify-center rounded-full bg-transparent border-2 border-white text-white"
-                            >
-                                {/* Right Arrow SVG */}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
-                                    <g clip-path="url(#clip0_327_4536)">
-                                        <path d="M25 0C11.1932 0 0 11.1932 0 25C0 38.8068 11.1932 50 25 50C38.8068 50 50 38.8068 50 25C50 11.1932 38.8068 0 25 0ZM25 45.8333C13.4943 45.8333 4.1667 36.5057 4.1667 25C4.1667 13.4943 13.4943 4.1667 25 4.1667C36.5057 4.1667 45.8333 13.4943 45.8333 25C45.8333 36.5057 36.5057 45.8333 25 45.8333Z" fill="white" />
-                                        <path d="M22.3064 13.1102C21.4929 12.2966 20.1737 12.2966 19.3602 13.1102C18.5466 13.9238 18.5466 15.2429 19.3602 16.0565L28.3037 25L19.3602 33.9436C18.5466 34.7572 18.5466 36.0763 19.3602 36.8899C20.1737 37.7034 21.4929 37.7034 22.3064 36.8899L32.7231 26.4732C33.5367 25.6596 33.5367 24.3405 32.7231 23.5269L22.3064 13.1102Z" fill="white" />
-                                    </g>
-                                    <defs>
-                                        <clipPath id="clip0_327_4536">
-                                            <rect width="50" height="50" fill="white" />
-                                        </clipPath>
-                                    </defs>
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-
-
-                    {/* Text Section */}
-                    <div className="flex flex-col">
-                        <h4 className="text-white text-2xl font-light"
-                            style={{
-                                color: '#fff',
-                                fontFamily: 'Work Sans',
-                                fontSize: '24px',
-                                fontStyle: 'normal',
-                                fontWeight: '400',
-                                lineHeight: 'normal'
-                            }}
-                        >Testimonials</h4>
-                        <h2 className="text-white text-4xl font-semibold mb-6 max-w-[550px]"
-                            style={{
-                                color: '#fff',
-                                fontFamily: 'Avenir LT Std',
-                                fontSize: '40px',
-                                fontStyle: 'normal',
-                                fontWeight: '600',
-                                lineHeight: '53px',
-                                maxWidth: '550px',
-
-                            }}
-                        >
-                            HEAR WHAT OUR ALUMNI SAY ABOUT BIMT
-                        </h2>
-                        <p
-                            className="text-white text-xl leading-relaxed mb-8 max-w-[600px]"
-                            style={{
-                                overflow: 'hidden',
-                                color: '#fff',
-                                fontFamily: 'Work Sans',
-                                fontSize: '36px',
-                                fontStyle: 'normal',
-                                fontWeight: '400',
-                                lineHeight: '35px',
-                                maxWidth: '600px',
-                                whiteSpace: 'normal',
-                                wordBreak: 'keep-all',
-                            }}
-                        >
-                            {text}
-                        </p>
-                        <button className="border-2 border-white text-white text-lg font-semibold py-2 px-6 rounded-lg hover:bg-white hover:text-blue-900 transition duration-300" style={{
-                            width: '234px',
-                            display: 'inline-flex',
-                            height: '56px',
-                            padding: '0px 20px',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            gap: '10px',
-                            flexShrink: '0'
-
-                        }}>
-                            {buttonLabel}
-                        </button>
-                    </div>
-
-                    <div style={{ paddingTop: '550px' }}>
-
-                        <button className="flex items-end border-2 border-[#fff] rounded-lg px-4 py-2 font-semibold text-[#fff] text-lg relative"
-                            style={{ fontFamily: 'Playfair', fontSize: '20px', fontWeight: '700', lineHeight: '24px', textAlign: 'left', textUnderlinePosition: 'from-font', textDecorationSkipInk: 'none' }}
-                        >
-                            {/* Icon with Rotation */}
-                            <div className="absolute -top-3 -left-6 transform -rotate-10">
-                                <Image src="/images/graduate 1 (2).png"
-                                    width={61.25}
-                                    height={43.04}
-                                    alt='storeis'
-                                    style={{ transform: 'translate(-20%, -45%) rotate(-10.795deg)' }} />
-                            </div>
-                            {/* Button Text */}
-                            View all stories
-
-                        </button>
-                    </div>
-                </div>
-            </div>
 
         </>
     );
