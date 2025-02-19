23d2a785432a03a42721aa917bbfcfee32d216ed
Dinesh-Olutek <dinesh.olutek@gmail.com>
2025-02-19 14:30:04 +0530

    nextconfigsass

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,23 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["bimt.lk"], 
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        {
+          loader: "sass-loader",
+          options: {
+            implementation: require("sass"), // Use Dart Sass
+            sassOptions: {
+              fiber: false, // Prevents the legacy API warning
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
