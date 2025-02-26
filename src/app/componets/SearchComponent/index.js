2f121251d180c3db67107588ce7d9c49a291e940
Dinesh Garusinghe <ictdinesh.se@gmail.com>
2025-02-26 19:38:16 +0530

    filter

--- src/app/componets/SearchComponent/index.js
+++ src/app/componets/SearchComponent/index.js
@@ -80,27 +80,74 @@ const SearchPage = () => {
     return (
         <>
             {/* Hero Section */}
-            <div className="relative bg-cover bg-center h-[480px] z-0" style={{ backgroundImage: "url('/images/Rectangle 1.png')" }}>
-                <div className="relative max-w-[1430px] mx-auto top-[55px]">
+
+            {/* Hero Section */}
+            <div className="relative w-full bg-cover bg-top heroSectionHeight"
+                style={{ backgroundImage: "url('/images/6-2.jpg')" }}
+
+            >
+                {/* Breadcrumb Section Wrapper */}
+                <div className="relative max-w-[1450px] mx-auto top-[55px] px-10 py-50 bredcrumbSectionMain">
                     {/* Breadcrumb Section */}
-                    <div className="absolute container mx-auto px-6 lg:px-16 flex items-center h-full z-20">
-                        <nav className="bg-gray-200 bg-opacity-80 py-2 px-4 rounded-lg text-sm text-gray-700" style={{ background: '#ffffff45' }}>
+                    <div className="relative flex w-full max-w-[1450px] gap-5 px-2 py-12 breadCrumbSubSection">
+                        <nav
+                            className="py-2 px-4 text-sm text-gray-700"
+                            style={{
+                                display: "inline-flex",
+                                padding: "5px 25px",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                gap: "10px",
+                                background: "#FFFFFF30",
+                                borderRadius: '100px'
+                            }}
+                        >
                             <ol className="flex space-x-2">
                                 <li>
-                                    <Link href="/" className="hover:underline text-white font-semibold">
+                                    <Link href="/" className="text-black breadcrumbFontWhite font-avenir">
                                         Home
                                     </Link>
                                 </li>
-                                <span className="text-gray-400">/</span>
+                                <span className="text-gray-400 arroSvgCrumPadding">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-[12px] h-[12px] svgRightArrowBreadCrumb"
+                                        viewBox="0 0 15 15"
+                                        fill="none"
+                                    >
+                                        <path
+                                            d="M11.667 6.9147L4.99178 0.239632C4.83739 0.0851213 4.6313 0 4.41154 0C4.19179 0 3.98569 0.0851213 3.83131 0.239632L3.33972 0.731092C3.01985 1.05133 3.01985 1.57182 3.33972 1.89157L8.94504 7.49689L3.3335 13.1084C3.17912 13.2629 3.09387 13.4689 3.09387 13.6885C3.09387 13.9084 3.17912 14.1144 3.3335 14.269L3.82509 14.7604C3.9796 14.9149 4.18557 15 4.40533 15C4.62508 15 4.83118 14.9149 4.98556 14.7604L11.667 8.0792C11.8217 7.9242 11.9067 7.71725 11.9062 7.49726C11.9067 7.2764 11.8217 7.06958 11.667 6.9147Z"
+                                            fill="white"
+                                        />
+                                    </svg>
+                                </span>
+
                                 <li>
-                                    <Link href="/Programs" className="hover:underline text-white font-semibold">
-                                        Programs
+                                    <Link href="/Search" className="breadcrumbFontWhite font-avenir">
+                                        Programmes
                                     </Link>
+                                </li>
+                                <span className="text-gray-400 arroSvgCrumPadding">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="w-[12px] h-[12px] svgRightArrowBreadCrumb"
+                                        viewBox="0 0 15 15"
+                                        fill="none"
+                                    >
+                                        <path
+                                            d="M11.667 6.9147L4.99178 0.239632C4.83739 0.0851213 4.6313 0 4.41154 0C4.19179 0 3.98569 0.0851213 3.83131 0.239632L3.33972 0.731092C3.01985 1.05133 3.01985 1.57182 3.33972 1.89157L8.94504 7.49689L3.3335 13.1084C3.17912 13.2629 3.09387 13.4689 3.09387 13.6885C3.09387 13.9084 3.17912 14.1144 3.3335 14.269L3.82509 14.7604C3.9796 14.9149 4.18557 15 4.40533 15C4.62508 15 4.83118 14.9149 4.98556 14.7604L11.667 8.0792C11.8217 7.9242 11.9067 7.71725 11.9062 7.49726C11.9067 7.2764 11.8217 7.06958 11.667 6.9147Z"
+                                            fill="white"
+                                        />
+                                    </svg>
+                                </span>
+                                <li>
+                                    <span className="breadcrumbFontWhite font-avenir">Search Results for:</span>
                                 </li>
                             </ol>
                         </nav>
                     </div>
                 </div>
+
 
                 {/* Search Bar */}
                 <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 z-30 flex justify-center px-4 md:px-0 searchProgram">
@@ -125,130 +172,79 @@ const SearchPage = () => {
             </div>
 
             {/* Main Section */}
-            <div className="container mt-[55px] mx-auto px-4 max-w-[1300px] mb-[110px]">
+            <div className="container mt-[55px] mx-auto px-4 max-w-[1420px] mb-[110px]">
                 {loading ? (
                     <div className="flex justify-center fullscreen-loader">
                         <div className="loader"></div>
                     </div>
-                ) : (<>
-                    <div className="py-8">
-                        <p className="text-gray-600 mt-4"
-                            style={{
-                                color: '#272A5D',
-                                fontFamily: 'Work Sans',
-                                fontSize: '24px',
-                                fontStyle: 'normal',
-                                fontWeight: '400',
-                                lineHeight: 'normal'
-                            }}
-                        >
-                            Search results for: <span className="font-semibold">{query}</span>
-                        </p>
-                    </div>
-
-                    <div className="flex gap-8">
-                        {/* Filters Section */}
-                        <div className="w-1/4 bg-white shadow-md p-4 rounded-md">
-                            <h2 className="text-lg font-semibold text-[#1D267D] mb-4"
+                ) : (
+                    <>
+                        {/* Filters Section - Moved to Top */}
+                        <div className="bg-white shadow-md p-4 rounded-md mb-6">
+                            <h2 className="text-lg font-semibold text-[#1D267D] text-avenir mb-4"
                                 style={{
                                     color: "#272A5D",
-                                    fontFamily: 'Avenir LT Std',
                                     fontSize: '30px',
                                     fontStyle: 'normal',
                                     fontWeight: '600',
-                                    lineHeight: '53px'
+                                    lineHeight: '53px',
+                                    paddingLeft:'20px',
+                                    paddingRight:'20px'
+
                                 }}
                             >Filters</h2>
-                            {/* Programs Filter */}
-                            <div className="mb-4">
-                                <label className="block text-gray-700 font-medium mb-2"
+
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center md:max-w-[600px] mx-auto">
+                                {/* Programs Filter */}
+                                <label className="text-gray-700 font-medium text-avenir"
                                     style={{
                                         color: "#272A5D",
-                                        fontFamily: 'Avenir LT Std',
                                         fontSize: '20px',
                                         fontStyle: 'normal',
                                         fontWeight: '400',
                                         lineHeight: '53px'
-                                    }}
-                                >Programs</label>
-                                <select className="w-full border rounded-md p-2" onChange={(e) => handleFilterChange('program', e.target.value)} value={filters.program}>
+                                    }}>Select your Favorite Programs</label>
+                                <select className="border rounded-md p-2 w-full md:w-auto" onChange={(e) => handleFilterChange('program', e.target.value)} value={filters.program}>
                                     <option value="">All</option>
                                     {programOptions.map((program, index) => (
-                                        <option key={index} value={program}>
-                                            {program}
-                                        </option>
+                                        <option key={index} value={program}>{program}</option>
                                     ))}
                                 </select>
                             </div>
-                            {/* Method Filter */}
-                            <div className="mb-4">
-                                <label className="block text-gray-700 font-medium mb-2" style={{
-                                    color: "#272A5D",
-                                    fontFamily: 'Avenir LT Std',
-                                    fontSize: '20px',
-                                    fontStyle: 'normal',
-                                    fontWeight: '400',
-                                    lineHeight: '53px'
-                                }}>Method</label>
-                                <select className="w-full border rounded-md p-2" onChange={(e) => handleFilterChange('method', e.target.value)} value={filters.method}>
-                                    <option value="">All</option>
-                                    <option value="Online">Online</option>
-                                    <option value="Offline">Offline</option>
-                                </select>
-                            </div>
-                            {/* Level Filter */}
-                            <div className="mb-4">
-                                <label className="block text-gray-700 font-medium mb-2" style={{
-                                    color: "#272A5D",
-                                    fontFamily: 'Avenir LT Std',
-                                    fontSize: '20px',
-                                    fontStyle: 'normal',
-                                    fontWeight: '400',
-                                    lineHeight: '53px'
-                                }}>Level</label>
-                                <select className="w-full border rounded-md p-2" onChange={(e) => handleFilterChange('level', e.target.value)} value={filters.level}>
-                                    <option value="">All</option>
-                                    <option value="Undergraduate">Undergraduate</option>
-                                    <option value="Postgraduate">Postgraduate</option>
-                                </select>
-                            </div>
+
 
                         </div>
 
                         {/* Results Section */}
-                        <div className="w-3/4">
-                            <h2
-                                className="text-lg font-semibold text-[#1D267D] mb-4"
+                        <div>
+                            <h2 className="text-lg font-semibold text-[#1D267D] mb-4 text-avenir"
                                 style={{
                                     color: '#272A5D',
-                                    fontFamily: 'Work Sans',
+
                                     fontSize: '24px',
                                     fontStyle: 'normal',
                                     fontWeight: '400',
                                     lineHeight: 'normal',
-                                }}
-                            >{`${filteredCourses.length} results for '${query}'`}</h2>
+                                }}>
+                                {`${filteredCourses.length} results for '${query}'`}
+                            </h2>
 
-                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
                                 {filteredCourses.map((course) => (
                                     <div
                                         key={course._id}
                                         className="bg-white shadow-md p-4 rounded-md transform transition duration-300 hover:-translate-y-2 hover:shadow-2xl cursor-pointer"
-                                        style={{
-                                            transition: "all 0.3s ease",
-                                        }}
                                         onClick={() => handleViewDetails(course.courseID)}
                                     >
                                         <img
                                             src={course.courseImg}
                                             alt={course.courseName}
-                                            className="w-full h-40 object-cover rounded-md mb-4"
+                                            className="w-full h-40 object-cover mb-4"
                                         />
                                         <h3
-                                            className="text-lg font-semibold text-[#1D267D]"
+                                            className="text-lg font-semibold text-[#1D267D] text-avenir"
                                             style={{
                                                 color: '#272A5D',
-                                                fontFamily: "Avenir LT Std",
                                                 fontSize: '20px',
                                                 fontStyle: 'normal',
                                                 fontWeight: '600',
@@ -268,15 +264,31 @@ const SearchPage = () => {
                             </div>
                         </div>
 
-                    </div >
-                    <div className="flex justify-center mt-8">
-                        <button disabled={currentPage === 1} onClick={() => setCurrentPage(currentPage - 1)}>Previous</button>
-                        <span>{`Page ${currentPage} of ${totalPages}`}</span>
-                        <button disabled={currentPage === totalPages} onClick={() => setCurrentPage(currentPage + 1)}>Next</button>
-                    </div>
-                </>
+                        {/* Pagination */}
+                        <div className="hadow-md p-4 rounded-md mb-6 max-w-[800px] mx-auto">
+                            <div className="flex justify-center mt-8">
+                                <button
+                                    className={`py-2 px-4 rounded-md transition duration-300 ${currentPage === 1 ? 'opacity-50 cursor-not-allowed' : 'bg-[#272A5D] text-white hover:bg-white hover:text-[#272A5D]'}`}
+                                    disabled={currentPage === 1}
+                                    onClick={() => setCurrentPage(currentPage - 1)}
+                                >
+                                    Previous
+                                </button>
+                                <span className="mx-4">{`Page ${currentPage} of ${totalPages}`}</span>
+                                <button
+                                    className={`py-2 px-4 rounded-md transition duration-300 ${currentPage === totalPages ? 'opacity-50 cursor-not-allowed' : 'bg-[#272A5D] text-white hover:bg-white hover:text-[#272A5D]'}`}
+                                    disabled={currentPage === totalPages}
+                                    onClick={() => setCurrentPage(currentPage + 1)}
+                                >
+                                    Next
+                                </button>
+                            </div>
+                        </div>
+
+                    </>
                 )}
-            </div >
+            </div>
+
 
             <style jsx>{`
   .fullscreen-loader {
